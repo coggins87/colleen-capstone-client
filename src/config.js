4d6c0da33cae5c9f7466acf168cda70af8a04066
Colleen <colleenhiggins87@gmail.com>
2019-10-08 17:17:44 -0700

    tests updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-/*   API_ENDPOINT: "https://obscure-cliffs-35805.herokuapp.com/api",
- */  TOKEN_KEY: "amrap-client-auth-token",
- API_ENDPOINT: "http://localhost:8000/api",
-  API_KEY: process.env.AMRAP_API_KEY
+   API_ENDPOINT: "https://obscure-cliffs-35805.herokuapp.com/api",
+   TOKEN_KEY: "amrap-client-auth-token",
+/*  API_ENDPOINT: "http://localhost:8000/api",
+ */  API_KEY: process.env.AMRAP_API_KEY
 };
